0bc12bc2b88a2c71cfed7aa83d178ff02314573f
Rawhate <49337637+Rawhate@users.noreply.github.com>
2019-04-06 17:13:58 +0600

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -7,8 +7,8 @@
 // @match        https://pixelzone.io/*
 // @match        http://pixelzone.io/*
 // @homepage     ~~~
-// @updateURL    https://raw.githubusercontent.com/Pinkfloydd/ArgentinaMap_PixelZone/blob/master/minimap.user.js
-// @downloadURL  https://raw.githubusercontent.com/Pinkfloydd/ArgentinaMap_PixelZone/blob/master/minimap.user.js
+// @updateURL    https://raw.githubusercontent.com/Rawhate/ArgentinaMap_PixelZone/blob/master/minimap.user.js
+// @downloadURL  https://raw.githubusercontent.com/Rawhate/ArgentinaMap_PixelZone/blob/master/minimap.user.js
 // @grant        none
 // ==/UserScript==
 
@@ -18,7 +18,7 @@ Number.prototype.between = function(a, b) {
   return this > min && this < max;
 };
 var range = 25;
-window.baseTepmlateUrl = 'https://raw.githubusercontent.com/Pinkfloydd/ArgentinaMap_PixelZone/master/';
+window.baseTepmlateUrl = 'https://raw.githubusercontent.com/Rawhate/ArgentinaMap_PixelZone/master/';
 
 window.addEventListener('load', function () {
     //Regular Expression to get coordinates out of URL
